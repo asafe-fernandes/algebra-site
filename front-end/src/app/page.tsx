a4114be700e048a942c0bd18a73b3f0719af4f5a
rgmenezes <faelgmp@gmail.com>
2024-12-06 21:51:46 -0300

    iniciando front em next

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function lendingPage(){
+  return(
+    <main>
+      <h1>Teste de textos</h1><br />
+
+      <h1>Título H1</h1>
+      <h2>Título H2</h2>
+      <h3>Título H3</h3>
+      <h4>Título H4</h4>
+      <h5>Destaque</h5>
+      <p>Parágrafo</p>
+      <p className="p_small">Parágrafo menor</p>
+    </main>
+  );
+};
