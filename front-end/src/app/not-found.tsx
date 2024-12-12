a14efc6dfd6cfb04cb7e8add3b4466941f428161
rgmenezes <faelgmp@gmail.com>
2024-12-12 12:54:54 -0300

    alterando acao de voltar da pagina notfound

--- front-end/src/app/not-found.tsx
+++ front-end/src/app/not-found.tsx
@@ -1,12 +1,16 @@
-import LinkButton from '@comp/actions/LinkButton';
+'use client';
+
 import styles from './not-found.module.css';
 import { BsArrowReturnLeft } from 'react-icons/bs';
+import Button from '@comp/actions/Button';
+import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
+  const router = useRouter();
   return (
     <section className={styles.sectionContainer}>
       <h1>Página não encontrada!<br/><strong>[404]</strong></h1>
-      <LinkButton href="/">Voltar <BsArrowReturnLeft /></LinkButton>
+      <Button onClick={() => router.back()}>Voltar <BsArrowReturnLeft /></Button>
     </section>
   );
 }
