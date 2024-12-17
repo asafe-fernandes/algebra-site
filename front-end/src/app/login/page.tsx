ba65fc21029295432f0bf503570da67dbc96920e
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2024-12-16 23:07:51 -0300

    creating login page

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -1,15 +1,24 @@
+'use client';
+
 import Header from '@mod/layout/Header';
 import Footer from '@mod/layout/Footer';
 import styles from './page.module.css';
+import { useState } from 'react';
+import Theme from '@cont/Theme';
+import LoginForm from '@mod/form/LoginForm';
+import Section from '@cont/Section';
 
 export default function Login(){
+  const [theme, setTheme] = useState<string>('light');
   return(
-    <>
-      <Header />
+    <Theme theme={theme == 'light' || theme == 'dark' ? theme : undefined}>
+      <Header onChangeTheme={setTheme}/>
       <main className={styles.mainContainer}>
-
+        <Section>
+          <LoginForm />
+        </Section>
       </main>
       <Footer />
-    </>
+    </Theme>
   );
 }
--- front-end/src/components/actions/ButtonIcon.tsx
+++ front-end/src/components/actions/ButtonIcon.tsx
@@ -4,10 +4,11 @@ import styles from './ButtonIcon.module.css';
 type TypeButton = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
   children: React.ReactNode
   detach?: boolean
+  className: string
 }
 
-export default function ButtonIcon({children, detach, ...props}: TypeButton){
+export default function ButtonIcon({children, detach, className, ...props}: TypeButton){
   return (
-    <button className={`${styles.buttonContainer} ${detach && styles.detach}`} {...props}>{children}</button>
+    <button className={`${className} ${styles.buttonContainer} ${detach && styles.detach}`} {...props}>{children}</button>
   );
 }
