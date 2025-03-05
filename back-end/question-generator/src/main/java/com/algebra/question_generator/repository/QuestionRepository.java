b5fcb56169ebdd63ebf23e9c117fd750409efd1d
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-03-05 14:20:21 -0300

    created Question repository and its service

--- back-end/question-generator/src/main/java/com/algebra/question_generator/repository/QuestionRepository.java
+++ back-end/question-generator/src/main/java/com/algebra/question_generator/repository/QuestionRepository.java
@@ -1,10 +1,15 @@
 package com.algebra.question_generator.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.algebra.question_generator.model.Question;
 
 @Repository
 public interface QuestionRepository extends MongoRepository<Question, String> {
 
+    @Query("{'rating': {$gte: ?0, $lte: ?1}}")
+    List<Question> findQuestionByRating(double minRating, double maxRating);
 }
