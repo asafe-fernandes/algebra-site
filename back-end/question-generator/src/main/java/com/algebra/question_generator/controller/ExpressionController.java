2619fdd27238eaf2d134357b970415a1bec60040
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-03-05 14:19:18 -0300

    added new endpoints

--- back-end/question-generator/src/main/java/com/algebra/question_generator/controller/ExpressionController.java
+++ back-end/question-generator/src/main/java/com/algebra/question_generator/controller/ExpressionController.java
@@ -1,28 +1,52 @@
 package com.algebra.question_generator.controller;
 
 import com.algebra.question_generator.model.Question;
-import com.algebra.question_generator.model.QuestionRequestDTO;
+import com.algebra.question_generator.model.DTOs.*;
 import com.algebra.question_generator.service.ExpressionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.algebra.question_generator.service.QuestionService;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/expressions")
+@RequestMapping("/api")
 public class ExpressionController {
 
-  @Autowired
-  private ExpressionService expressionService;
+    private ExpressionService expressionService;
+    private QuestionService questionService;
 
-  @GetMapping
-  public ResponseEntity<Question> generateExpression(@RequestBody QuestionRequestDTO questionRequestDTO) {
+    public ExpressionController(ExpressionService expressionService, QuestionService questionService) {
+        this.expressionService = expressionService;
+        this.questionService = questionService;
+    }
 
-    Question response = expressionService.generateRandomExpression(questionRequestDTO);
-    return ResponseEntity.ok().body(response);
-  }
+    @GetMapping("/generate")
+    public ResponseEntity<Question> generateExpression(@RequestBody QuestionRequestDTO questionRequestDTO) {
+        Question response = expressionService.generateRandomExpression(questionRequestDTO);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Question> getQuestion(@RequestBody GetRequestDTO getRequestDTO) {
+        Question response = questionService.getByRating(getRequestDTO);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Question>> getAll() {
+        List<Question> response = questionService.getAll();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Question> insert(@RequestBody QuestionDTO questionDTO) {
+        Question response = questionService.insert(questionDTO);
+        return ResponseEntity.ok().body(response);
+    }
 }
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
