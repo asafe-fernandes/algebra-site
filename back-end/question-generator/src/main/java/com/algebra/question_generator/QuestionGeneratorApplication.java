7c4696d12970027eb6a423e501fa988e0390cf04
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-03-11 11:38:01 -0300

    added dotenv to app

--- back-end/question-generator/src/main/java/com/algebra/question_generator/QuestionGeneratorApplication.java
+++ back-end/question-generator/src/main/java/com/algebra/question_generator/QuestionGeneratorApplication.java
@@ -11,6 +11,8 @@ public class QuestionGeneratorApplication {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("DB_URI", dotenv.get("DB_URI"));
+        System.setProperty("KEYSTORE_PATH", dotenv.get("KEYSTORE_PATH"));
+        System.setProperty("KEYSTORE_PASSWORD", dotenv.get("KEYSTORE_PASSWORD"));
 
         SpringApplication.run(QuestionGeneratorApplication.class, args);
 
