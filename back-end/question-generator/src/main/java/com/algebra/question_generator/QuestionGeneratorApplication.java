01a77a68428aecec5c90223d570611eda6edf1dc
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-03-23 03:11:32 -0300

    finnaly fixed the issue with the dotenv class not loading before spring starting

--- back-end/question-generator/src/main/java/com/algebra/question_generator/QuestionGeneratorApplication.java
+++ back-end/question-generator/src/main/java/com/algebra/question_generator/QuestionGeneratorApplication.java
@@ -3,10 +3,13 @@ package com.algebra.question_generator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.algebra.question_generator.config.DotEnvConfig;
+
 @SpringBootApplication
 public class QuestionGeneratorApplication {
 
     public static void main(String[] args) {
+        new DotEnvConfig();
         SpringApplication.run(QuestionGeneratorApplication.class, args);
     }
 
